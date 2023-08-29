07feb525244907ebedf326643c08caf87087402c
PyoChangYeong <game1425@naver.com>
2023-08-29 16:51:12 +0900

    fix: Member GeneratedValue타입 수정 및 yml에 Redis 추가

--- LunchBoxDonation/src/main/java/com/lunchbox/lunchboxdonation/entity/member/Member.java
+++ LunchBoxDonation/src/main/java/com/lunchbox/lunchboxdonation/entity/member/Member.java
@@ -14,21 +14,20 @@ import javax.persistence.*;
 public class Member extends Timestamp {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue
     private Long id;
 
-    @NonNull
-    @Column(unique = true)
+    @Column(unique = true,nullable = false)
     private String memberId;
 
-    @NonNull
-    @Column
+    @Column(unique = true,nullable = false)
     private String memberPw;
 
-    @NonNull
-    @Column
+    @Column(unique = true,nullable = false)
     private String memberName;
+    @Column(unique = true,nullable = false)
     private String memberEmail;
+    @Column(unique = true,nullable = false)
     private String memberPhoneNumber;
 //    private LocalDateTime modDate;
 
